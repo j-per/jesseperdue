c799fd8ce71bb2b97a7ee16135a3e56e9a52bfa4
Jesse Perdue <perdue.jesse@gmail.com>
2021-04-15 20:03:28 -0700

    Removed Gatsby SSR file

--- gatsby-ssr.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { GlobalStyles } from './src/styles/GlobalStyles';
-
-export const wrapPageElement = ({ element }) => (
-  <>
-    <GlobalStyles />
-    {element}
-  </>
-);
